66f120b0ef4f805fad12b4829b153a0289a901ae
Aditya6122 <78961497+Aditya6122@users.noreply.github.com>
2023-05-05 22:31:31 +0530

    Add files via upload

---
+++ model_check.py
@@ -0,0 +1,19 @@
+import cv2
+from PIL import Image
+import numpy as np
+import pickle
+
+scaler = pickle.load(open('model\standardscaler.pkl','rb'))
+model = pickle.load(open('model\model.pkl','rb'))
+
+img = cv2.imread("output.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh1 = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
+X_data = thresh1/255
+X_data = scaler.transform(X_data)
+X_data = X_data.reshape((-1,784))
+prediction = model.predict(X_data)
+
+print(prediction)
+
+cv2.imwrite("image_cv2.jpg", thresh1)
