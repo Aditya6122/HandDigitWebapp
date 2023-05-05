ca8e2d6bcd41f81e227282f98b4d4967f1a9ec6e
Aditya6122 <78961497+Aditya6122@users.noreply.github.com>
2023-05-05 22:34:06 +0530

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,73 @@
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = 300;
+canvas.height = 300;
+
+let isPainting = false;
+let lineWidth = 10;
+let startX;
+let startY;
+
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'submit'){
+        var image = canvas.toDataURL('image/jpg');
+        $.ajax({
+            url:"http://127.0.0.1:8000/",
+            type:"POST",
+            contentType: "application/json",
+            data: JSON.stringify(image),
+            success: function(response){
+                var k=response;
+                const node = document.getElementById("answer");
+                node.textContent=String(k)
+            }
+        });
+    }
+})
+
+toolbar.addEventListener('change', e => {
+    if(e.target.id === 'stroke') {
+        ctx.strokeStyle = e.target.value;
+    }
+
+    if(e.target.id === 'lineWidth') {
+        lineWidth = e.target.value;
+    }
+    
+});
+
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX - canvasOffsetX -5 , e.clientY - 70);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
